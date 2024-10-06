8f0fcc3694242718f04000a8ae0e6af00c2cd523
Katerina Slavova <katerinaslavova05@gmail.com>
2024-10-06 22:02:30 +0200

    Add files via upload

---
+++ ICT and Software Engineering/Week5/Updates and styling/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+from random import uniform
+
+def random_light():
+    light=uniform(0,1000)
+    return "The light is " + str(round(light,2)) + "W/m^2."
+
+def current_time():
+    rightNow = datetime.now()
+    time = rightNow.strftime("%I:%M%p")
+    time = time.lstrip('0')
+    time = time.lower()
+    day = rightNow.strftime("%A")
+
+    return "It is " + time + " on " + day + "."
+
+app = Flask(__name__)
+
+@app.route('/')
+def display():
+    return render_template("timeAndLight.html",
+                            time=current_time(),
+                            light=random_light())
+
+if __name__ == '__main__':
+    app.run(port=5000)
